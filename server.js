3550d5a555036b5ddec000f0e76eada8f206bb9f
jnavarro30 <jnavarro30@live.com>
2021-10-01 00:36:28 -0700

    prepare for deployment

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+const path = require('path')
 
 app.use(express.json())
 
@@ -29,6 +30,16 @@ mongoose
     .connect(db)
     .then(() => console.log('Database connection established...'))
     .catch(err => console.log(err))
+
+// serve static assets if in production
+if (process.env.NODE_ENV === 'production') {
+    // set static folder
+    app.use(express.static('client/build'))
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    })
+}
 
 // run server
 const port = process.env.PORT || 5000
